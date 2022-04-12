962d9408ee95598933eba447f9b57e4b56ac90fe
alan <alanmorais1503@gmail.com>
2022-04-12 10:14:18 -0300

    created menu item delivery locales

---
+++ ragu-web-app/src/app/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+
+@Component({
+  selector: 'ragu-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  items: MenuItem[];
+
+  constructor() { 
+    this.items = [{
+      label: 'Locais de entrega',
+      icon: PrimeIcons.MAP
+    }]
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
