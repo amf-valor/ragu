2923695feed66f5a4a88d170465a343111b7e07a
alan <alanmorais1503@gmail.com>
2022-05-11 13:20:06 -0300

    created delivery locales migrations

--- ragu-web-api/Ragu.WebApi/Program.cs
+++ ragu-web-api/Ragu.WebApi/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Ragu.InfraStructure.Data;
+using Ragu.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<RaguDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RaguCs"));
+});
+
+builder.Services.AddScoped<DeliveryLocaleService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,8 +25,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
