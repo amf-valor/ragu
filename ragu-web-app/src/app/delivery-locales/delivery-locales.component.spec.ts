20562a0c030488f973e03cc31451fe63274cbb9a
alan <alanmorais1503@gmail.com>
2022-05-25 12:53:42 -0300

    WebApp delivery locales component test fix code identation

--- ragu-web-app/src/app/delivery-locales/delivery-locales.component.spec.ts
+++ ragu-web-app/src/app/delivery-locales/delivery-locales.component.spec.ts
@@ -84,22 +84,26 @@ describe('DeliveryLocalesComponent', () => {
     expect(page.saveButton.disabled).toBeTrue();
   });
 
-  it('GIVEN tax input WHEN user types any letter THEN tax input should not accept and remain empty', async () => {
+  it(`GIVEN tax input WHEN user types any letter 
+      THEN tax input should not accept and remain empty`, async () => {
+
     await userEvent.type(page.taxInput, 'as');
     expect(page.taxInput.textContent).toBe('');
   });
 
-  it('GIVEN tax input WHEN user types a number THEN tax input should format the number to currency real', async () => {
+  it(`GIVEN tax input WHEN user types a number 
+      THEN tax input should format the number to currency real`, async () => {
+    
     fireEvent.change(page.taxInput, {target:{value: "20"}});
     fireEvent.blur(page.taxInput);
-
     //the replace is needed because the value is an HTML string from input
     //angular pipe, after formating, it turns to R$&nbsp20,00 which was 
     //breaking the comparision against R$ 20,00 and failing the test silently.
     expect(replaceNbspByEmptySpace(page.taxInput.value)).toEqual('R$ 20,00');
   });
 
-  it('GIVEN neighbourhood and tax WHEN user clicks on salvar THEN should clear inputs and append new line to the hood list ', waitForAsync(async() => {
+  it(`GIVEN neighbourhood and tax WHEN user clicks on salvar 
+      THEN should clear inputs and append new line to the hood list`, waitForAsync(async() => {
     expect(page.queryCell('Vila matilde')).toBeNull();
     
     await userEvent.type(page.hoodInput, 'Vila matilde');
@@ -116,71 +120,104 @@ describe('DeliveryLocalesComponent', () => {
     expect(screen.getByText('R$ 20,00')).toBeDefined();
   }));
 
-  it('GIVEN hood input WHEN do not provide the neighborhood THEN required error message appear', async () => {
-    expect(screen.queryByText('Bairro é obrigatório')).withContext('hood input required error message').toBeNull();
+  it(`GIVEN hood input WHEN do not provide the neighborhood 
+      THEN required error message appear`, async () => {
+    
+    expect(screen.queryByText('Bairro é obrigatório'))
+      .withContext('hood input required error message').toBeNull();
 
     await userEvent.type(page.hoodInput, 'any');
     await userEvent.clear(page.hoodInput);
     fixture.detectChanges();
 
-    expect(screen.queryByText('Bairro é obrigatório')).withContext('hood input required error message').not.toBeNull();
+    expect(screen.queryByText('Bairro é obrigatório'))
+      .withContext('hood input required error message').not.toBeNull();
   });
 
-  it('GIVEN tax input WHEN user do not provide the tax THEN required error message should appear', async () => {
-    expect(screen.queryByText('Taxa é obrigatório')).withContext('tax input required error message').toBeNull();
+  it(`GIVEN tax input WHEN user do not provide the tax 
+      THEN required error message should appear`, async () => {
+    
+    expect(screen.queryByText('Taxa é obrigatório'))
+      .withContext('tax input required error message').toBeNull();
 
     fireEvent.blur(page.taxInput);
     fixture.detectChanges();
 
-    expect(screen.queryByText('Taxa é obrigatório')).withContext('tax input required error message').not.toBeNull();
+    expect(screen.queryByText('Taxa é obrigatório'))
+      .withContext('tax input required error message').not.toBeNull();
   });
 
-  it('GIVEN save button was clicked WHEN is saving THEN load spinner should appear and disappear', async () => {
-    expect(page.saveButton.querySelector(SPINNER_CLASS)).withContext('save button loading when component appear').toBeNull();
+  it(`GIVEN save button was clicked WHEN is saving 
+      THEN load spinner should appear and disappear`, async () => {
 
-    await userEvent.type(screen.getByLabelText('Bairro'), 'any');
+    expect(page.saveButton.querySelector(SPINNER_CLASS))
+      .withContext('save button loading when component appear').toBeNull();
+
+    await userEvent.type(page.hoodInput, 'any');
     fireEvent.change(page.taxInput, {target:{value: "2"}});
     fireEvent.blur(page.taxInput);
     fixture.detectChanges();
     await userEvent.click(page.saveButton);
     fixture.detectChanges();
-    expect(page.saveButton.querySelector(SPINNER_CLASS)).withContext('save button loading just after clicked').not.toBeNull();
+    
+    expect(page.saveButton.querySelector(SPINNER_CLASS))
+      .withContext('save button loading just after clicked').not.toBeNull();
     
     await fixture.whenStable();
     fixture.detectChanges();
-    expect(page.saveButton.querySelector(SPINNER_CLASS)).withContext('save button loading when save operation finished').toBeNull();
+    
+    expect(page.saveButton.querySelector(SPINNER_CLASS))
+      .withContext('save button loading when save operation finished').toBeNull();
   });
 
-  it('GIVEN delivery locales previously registered WHEN user goes to Locais de entrega THEN all of them should be displayed', async() => {
+  it(`GIVEN delivery locales previously registered WHEN user goes to Locais de entrega 
+      THEN all of them should be displayed`, async() => {
+    
     await fixture.whenStable();
     fixture.detectChanges();
 
-    expect(page.getCell('itagua - in memory')).withContext('itagua should be displayed').not.toBeNull();
-    expect(page.getCell('centro - in memory')).withContext('centro should be displayed').not.toBeNull();
+    expect(page.getCell('itagua - in memory'))
+      .withContext('itagua should be displayed').not.toBeNull();
+    
+      expect(page.getCell('centro - in memory'))
+      .withContext('centro should be displayed').not.toBeNull();
   });
 
-  it('GIVEN component is initializing WHEN is fetching all delivery locales THEN', async () => {
+  it(`GIVEN component is initializing WHEN is fetching all delivery locales 
+      THEN loading should appear and disappear`, async () => {
+
     fixture.detectChanges();
-    expect(page.table.querySelector(SPINNER_CLASS)).withContext("loading should appear when is fetching").not.toBeNull();
+    
+    expect(page.table.querySelector(SPINNER_CLASS))
+      .withContext("loading should appear when is fetching").not.toBeNull();
 
     await fixture.whenStable();
     fixture.detectChanges();
-    expect(page.table.querySelector(SPINNER_CLASS)).withContext("loading should disappear after get all delivery locales").toBeNull();
+    
+    expect(page.table.querySelector(SPINNER_CLASS))
+      .withContext("loading should disappear after get all delivery locales").toBeNull();
   });
 
-  it('GIVEN some error ocurred WHEN save new delivery locale THEN isSaving should be false', () => {
-    spyOn(TestBed.inject(DeliveryLocalesService), 'post').and.returnValue(throwError(() => new Error("any")));
+  it(`GIVEN some error ocurred WHEN save new delivery locale 
+      THEN isSaving should be false`, () => {
+    
+    spyOn(TestBed.inject(DeliveryLocalesService), 'post')
+      .and.returnValue(throwError(() => new Error("any")));
     
     component.onSaveButtonClick();
 
     expect(component.isSaving).toBeFalse();
   });
 
-  it('GIVEN some error ocurred WHEN fetching delivery locales on init THEN should add error to MessageService', () => {
+  it(`GIVEN some error ocurred WHEN fetching delivery locales on init 
+      THEN should add error to MessageService`, () => {
+
     const messageService = TestBed.inject(MessageService);
     let actual: Message = {};
     
-    spyOn(TestBed.inject(DeliveryLocalesService), GET_ALL).and.returnValue(throwError(() =>  new Error("any")));
+    spyOn(TestBed.inject(DeliveryLocalesService), GET_ALL)
+      .and.returnValue(throwError(() =>  new Error("any")));
+    
     messageService.messageObserver.subscribe(message => actual = message as Message);
     
     component.ngOnInit();
@@ -188,15 +225,20 @@ describe('DeliveryLocalesComponent', () => {
     expect(actual).toEqual(ERROR_MESSAGE);
   });
 
-  it('GIVEN some error ocurred WHEN fetching all delivery locales on init THEN isTableLoading should be false', () => {
-    spyOn(TestBed.inject(DeliveryLocalesService), GET_ALL).and.returnValue(throwError(() =>  new Error("any")));
+  it(`GIVEN some error ocurred WHEN fetching all delivery locales on init 
+      THEN isTableLoading should be false`, () => {
+
+    spyOn(TestBed.inject(DeliveryLocalesService), GET_ALL)
+      .and.returnValue(throwError(() =>  new Error("any")));
 
     component.ngOnInit();
 
     expect(component.isTableLoading).toBeFalse();
   });
 
-  it('GIVEN an existing delivery locale WHEN user click on trash button THEN delivery locale should be removed from the hood list', async () => {
+  it(`GIVEN an existing delivery locale WHEN user click on trash button 
+      THEN delivery locale should be removed from the hood list`, async () => {
+
     await fixture.whenStable();
     fixture.detectChanges();
 
@@ -208,11 +250,15 @@ describe('DeliveryLocalesComponent', () => {
     expect(page.queryRow(ITAGUA_IN_MEMORY_PATTERN)).toBeNull();
   })
 
-  it('GIVEN some error ocurred WHEN deleting delivery locales THEN should add error to MessageService', () => {
+  it(`GIVEN some error ocurred WHEN deleting delivery locales 
+      THEN should add error to MessageService`, () => {
+
     const messageService = TestBed.inject(MessageService);
     let actual: Message = {};
     
-    spyOn(TestBed.inject(DeliveryLocalesService), 'delete').and.returnValue(throwError(() =>  new Error("any")));
+    spyOn(TestBed.inject(DeliveryLocalesService), 'delete')
+      .and.returnValue(throwError(() =>  new Error("any")));
+
     messageService.messageObserver.subscribe(message => actual = message as Message);
     
     component.onTrashButtonClick(0);
@@ -220,19 +266,24 @@ describe('DeliveryLocalesComponent', () => {
     expect(actual).toEqual(ERROR_MESSAGE);
   });
 
-  it('GIVEN trash button was clicked WHEN is removing THEN load spinner should appear and disappear', async () => {
+  it(`GIVEN trash button was clicked WHEN is removing 
+      THEN load spinner should appear and disappear`, async () => {
+
     await fixture.whenStable();
     fixture.detectChanges();
-    const trashButton = within(screen.getByRole('row', {name: /itagua - in memory/i})).getByTestId("trashButton")
-    const table = screen.getByTestId('delivery-locales-table');
+    const trashButton = page.getTrashButtonByRowName(ITAGUA_IN_MEMORY_PATTERN)
     
     await userEvent.click(trashButton);
     fixture.detectChanges();
-    expect(table.querySelector(SPINNER_CLASS)).withContext('table loading after trash button clicked').not.toBeNull();
+    
+    expect(page.table.querySelector(SPINNER_CLASS))
+      .withContext('table loading after trash button clicked').not.toBeNull();
     
     await fixture.whenStable();
     fixture.detectChanges();
-    expect(table.querySelector(SPINNER_CLASS)).withContext('table stop loading when delete operation finished').toBeNull();
+    
+    expect(page.table.querySelector(SPINNER_CLASS))
+      .withContext('table stop loading when delete operation finished').toBeNull();
   });
 
   function replaceNbspByEmptySpace(value: string): ArrayLike<string> {
