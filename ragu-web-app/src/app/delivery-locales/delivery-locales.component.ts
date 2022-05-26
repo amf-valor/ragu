3ed6df7b39a48ab6329b87bb068759765a2168c1
alan <alanmorais1503@gmail.com>
2022-05-26 09:14:30 -0300

    WebApp fixed subscription leaking in delivery locale component

--- ragu-web-app/src/app/delivery-locales/delivery-locales.component.ts
+++ ragu-web-app/src/app/delivery-locales/delivery-locales.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
-import { finalize } from 'rxjs';
+import { finalize, takeWhile } from 'rxjs';
 import { DeliveryLocale } from './delivery-locale.model';
 import { DeliveryLocalesService } from './delivery-locales.service';
 
@@ -11,38 +11,39 @@ import { DeliveryLocalesService } from './delivery-locales.service';
   templateUrl: './delivery-locales.component.html',
   styleUrls: ['./delivery-locales.component.css']
 })
-export class DeliveryLocalesComponent implements OnInit{
-
+export class DeliveryLocalesComponent implements OnInit, OnDestroy {
   deliveryLocalesForm: FormGroup;
-  
+
   private _deliveryLocales: DeliveryLocale[] = [];
   public get deliveryLocales(): DeliveryLocale[] {
     return this._deliveryLocales;
   }
-  
+
   private _isTableLoading: boolean;
   public get isTableLoading(): boolean {
     return this._isTableLoading;
   }
-  
-  public get hoodControl() : AbstractControl { 
-    return <AbstractControl>this.deliveryLocalesForm.get('hood'); 
-  }
-  
-  public get taxControl() : AbstractControl {
-    return <AbstractControl> this.deliveryLocalesForm.get('tax');
-  }
-  
+
   private _isSaving: boolean;
   public get isSaving(): boolean {
     return this._isSaving;
   }
 
+  public get hoodControl(): AbstractControl {
+    return <AbstractControl>this.deliveryLocalesForm.get('hood');
+  }
+  
+  public get taxControl(): AbstractControl {
+    return <AbstractControl>this.deliveryLocalesForm.get('tax');
+  }
+  
+  private _isAlive: boolean = true;
+
   constructor(
-    private formBuilder: FormBuilder, 
-    private deliveryLocalesService: DeliveryLocalesService, 
+    private formBuilder: FormBuilder,
+    private deliveryLocalesService: DeliveryLocalesService,
     private messageService: MessageService
-  ){ 
+  ) {
     this.deliveryLocalesForm = this.formBuilder.group({
       hood: ['', Validators.required],
       tax: [null, Validators.required]
@@ -54,10 +55,12 @@ export class DeliveryLocalesComponent implements OnInit{
 
   ngOnInit(): void {
     this._isTableLoading = true;
-    
+
     this.deliveryLocalesService.getAll()
-      .pipe(finalize(() => this._isTableLoading = false ))
-      .subscribe({
+      .pipe(
+        takeWhile(() => this._isAlive),
+        finalize(() => this._isTableLoading = false),
+      ).subscribe({
         next: deliveryLocales => {
           this._deliveryLocales = deliveryLocales;
         },
@@ -65,32 +68,37 @@ export class DeliveryLocalesComponent implements OnInit{
       });
   }
 
-  
-  onSaveButtonClick(){
+
+  onSaveButtonClick() {
     this._isSaving = true;
-    
+
     this.deliveryLocalesService.post({ hood: this.hoodControl.value, tax: this.taxControl.value })
-    .pipe(finalize(() => this._isSaving = false))
-    .subscribe({
-      next: deliveryLocale => {
+      .pipe(
+        finalize(() => this._isSaving = false),
+        takeWhile(() => this._isAlive)  
+      ).subscribe({
+        next: deliveryLocale => {
           this.deliveryLocales.push(deliveryLocale);
           this.deliveryLocalesForm.reset();
         },
         error: () => this.addErrorMessage()
       });
   }
-  
-  onTrashButtonClick(id: number){
+
+  onTrashButtonClick(id: number) {
     this._isTableLoading = true;
 
     this.deliveryLocalesService.delete(id)
-    .pipe(finalize(() => this._isTableLoading = false))
-    .subscribe({
-      next: () => this.removeDeliveryLocaleById(id),
-      error: () => this.addErrorMessage()
-    })
+      .pipe(
+        finalize(() => this._isTableLoading = false),
+        takeWhile(() => this._isAlive)
+      )
+      .subscribe({
+        next: () => this.removeDeliveryLocaleById(id),
+        error: () => this.addErrorMessage()
+      })
   }
-  
+
   private addErrorMessage(): void {
     this.messageService.add({
       severity: 'error',
@@ -104,5 +112,8 @@ export class DeliveryLocalesComponent implements OnInit{
     const indexToRemove = this.deliveryLocales.findIndex(item => item.id === id);
     this.deliveryLocales.splice(indexToRemove, quantityOfItemsToRemove);
   }
-  
+
+  ngOnDestroy(): void {
+    this._isAlive = false;
+  }
 }
