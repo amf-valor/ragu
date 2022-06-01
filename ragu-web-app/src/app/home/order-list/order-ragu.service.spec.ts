b371fc14f18f1d5738234963e6632e7cf639505f
alan <alanmorais1503@gmail.com>
2022-06-01 13:45:26 -0300

    WebApp moved helper stuff from order service spec to test module

--- ragu-web-app/src/app/home/order-list/order-ragu.service.spec.ts
+++ ragu-web-app/src/app/home/order-list/order-ragu.service.spec.ts
@@ -1,21 +1,10 @@
-import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { GENERIC_ERROR_MESSAGE, Mother } from 'src/testing';
+import { expectErrorWithMessage, GENERIC_ERROR_MESSAGE, HttpTestingControllerHelper, Mother } from 'src/testing';
 import { OrderService } from './order-ragu.service';
 
-fdescribe('OrderService', () => {
-  class HttpTestingControllerHelper {
-    constructor(private httpTestingController: HttpTestingController) {}
-
-    expectFirstStartsWith(url: string): TestRequest{
-      const testRequest = this.httpTestingController.match(request => request.url.startsWith(url))[0];
-      expect(testRequest).withContext(`it was not found any request for: ${url}`).toBeDefined();
-      return testRequest;
-    }
-  }
-
+describe('OrderService', () => {
   let orderService: OrderService;
   let httpTestingController: HttpTestingController;
   let httpTestingControllerHelper: HttpTestingControllerHelper;
@@ -58,11 +47,4 @@ fdescribe('OrderService', () => {
     
     testRequest.flush('', Mother.internalServerError());
   });
-
-  function expectErrorWithMessage(observable: Observable<unknown>, message: string){
-    observable.subscribe({
-      next: () => fail('expected error, not observable succeded'),
-      error: error => expect(error.message).toBe(message)
-    });
-  }
 });
--- ragu-web-app/src/testing/index.ts
+++ ragu-web-app/src/testing/index.ts
@@ -1,6 +1,25 @@
-import './component-fixture-extensions';
+import { HttpTestingController, TestRequest } from '@angular/common/http/testing';
+import { Observable } from 'rxjs';
+import './tests-extensions';
 import { Mother } from './mother';
 
 const GENERIC_ERROR_MESSAGE = "something unexpected occurred!";
 
-export{ GENERIC_ERROR_MESSAGE, Mother };
+function expectErrorWithMessage(observable: Observable<unknown>, message: string) {
+  observable.subscribe({
+    next: () => fail('expected error, not observable succeded'),
+    error: error => expect(error.message).toBe(message)
+  });
+}
+
+class HttpTestingControllerHelper {
+  constructor(private httpTestingController: HttpTestingController) {}
+
+  expectFirstStartsWith(url: string): TestRequest{
+    const testRequest = this.httpTestingController.match(request => request.url.startsWith(url))[0];
+    expect(testRequest).withContext(`it was not found any request for: ${url}`).toBeDefined();
+    return testRequest;
+  }
+}
+
+export { GENERIC_ERROR_MESSAGE, Mother, expectErrorWithMessage, HttpTestingControllerHelper };
